c2d18565a328aab19098c647d13cf3d27e672fe4
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-10-10 15:03:20 +0800

    增加一下关于tailwind css的注释

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',  /* 告诉tailwind css 这些路径下的 tailwind css 需要转换成原生的css */
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
